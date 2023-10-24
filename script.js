8a9acb3f197554fef4927654ee4556753c1b97bb
Matt Armstrong <matt@rfc20.org>
2023-10-23 17:13:10 -0700

    Add the getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("script.js loaded");
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const choice = getRandomInt(choices.length);
+    return choices[choice];
+}
